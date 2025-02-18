411c2646c6aca44021cccf52a31d5a64053728cc
Wonchae Lee <148836643+WonOfAKind@users.noreply.github.com>
2025-02-18 18:16:22 -0500

    Updated SET code

--- src/set_lidar.py
+++ src/set_lidar.py
@@ -3,7 +3,7 @@ import time
 
 def main():
     arduino = serial.Serial(port='COM4', baudrate=9600, timeout=0.1)
-    time.sleep(1)  # reset time
+    time.sleep(0.5)  # reset time
 
     while True:
         data = arduino.readline().decode('utf-8').strip()
