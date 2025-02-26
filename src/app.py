80dcae5874e4afd77692bca24c5ba1843f58a1b6
manav sanghvi <mannyjainlakes@gmail.com>
2025-02-25 19:05:36 -0500

    working?

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import numpy as np
 import sounddevice as sd
 import queue
 
-MODEL_PATH = "/app/audio_model.eim"
+MODEL_PATH = "/home/ufset/Desktop/SET_2024-25/src/audio_model.eim"
 
 # Queue for storing audio chunks
 audio_queue = queue.Queue()
@@ -19,19 +19,21 @@ def classify_audio(runner):
     model_info = runner.init()
     print(f"Model loaded: {model_info['project']['name']}")
 
+    labels = model_info['model_parameters']['labels']
+    
     while True:
         audio_data = audio_queue.get()  # Retrieve audio data from the queue
         audio_data = np.mean(audio_data, axis=1)  # Convert stereo to mono
 
-        features = runner.get_features_from_audio(audio_data)
-        result = runner.classify(features)
-
-        if "classification" in result["result"]:
-            print("Predictions:", result["result"]["classification"])
+        for res, audio in runner.classifier(0):
+            print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='\n')
+            for label in labels:
+               score = res['result']['classification'][label]
+               print('%s: %.2f\t' % (label, score), end='\n')
 
 def main():
     with AudioImpulseRunner(MODEL_PATH) as runner:
-        with sd.InputStream(callback=audio_callback, channels=1, samplerate=16000, blocksize=512):
+        with sd.InputStream(callback=audio_callback, channels=2, samplerate=44100, blocksize=512, device='default'):
             print("Listening... Press Ctrl+C to stop.")
             classify_audio(runner)  # Start classification loop
 
