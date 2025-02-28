661f024b69bfed608024d31a76f99db83e1b8b40
Amit "Sunny" Gupta <137424796+Amit-S-G@users.noreply.github.com>
2025-02-27 21:19:26 -0500

    Update AudioClassifier.py

--- src/AudioClassifier.py
+++ src/AudioClassifier.py
@@ -33,7 +33,7 @@ class AudioClassifier:
         audio_data = np.mean(audio_data, axis=1)  # Convert stereo to mono
 
         screaming_scores = []
-        for res, audio in runner.classifier(0):
+        for res, audio in runner.classifier(self.device_ID):
             print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='\n')
             for label in labels:
                 if label == 'notScreaming':
