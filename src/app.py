176edc506f4c2d48a8a5b8768e035d44db528fe9
Amit "Sunny" Gupta <137424796+Amit-S-G@users.noreply.github.com>
2025-03-13 20:54:19 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
+
 from AudioClassifier import AudioClassifier
 import sys
-
+'''
 def main():
     print(sd.query_devices())
     device_id = int(input("Which device ID would you like to use?"))
@@ -12,3 +13,12 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("\nStopped by user.")
+'''
+classifier = AudioClassifier(device_id=0)
+
+with AudioImpulseRunner(MODEL_PATH) as runner:
+    result = classifier.classify_audio(runner)
+    if result:
+        classification, score = result
+        if classification != "notScreaming" and score >= 0.7:
+            print("This is actually a scream post-processing.")
