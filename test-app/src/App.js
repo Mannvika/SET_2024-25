3c8d04cec3371d8fd3014c74cdb9487adc7cff91
MonishB123 <monishbeegamudre@gmail.com>
2025-04-22 19:35:05 -0400

    local host on frontend

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     const [imageSrc, setImageSrc] = useState(null);
 
     useEffect(() => {
-        const socket = io("192.168.113.26:8000", {
+        const socket = io("http://localhost:8000", {
             transports: ["websocket"],
             upgrade: false
         });
