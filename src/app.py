8a6dbd55b62e5be5fe9ba7590d68d1dec9c7ebe6
Amit "Sunny" Gupta <137424796+Amit-S-G@users.noreply.github.com>
2025-04-22 18:45:32 -0400

    Removed filtering code
    it didnt really work anyways

--- src/app.py
+++ src/app.py
@@ -103,13 +103,7 @@ def audio_classification_loop():
             print(f"Loaded model: {model_info['project']['owner']}/{model_info['project']['name']}")
             print(f"Window: {window_size} samples ({window_size/MODEL_SAMPLE_RATE:.2f}s)")
             for res, audio in runner.classifier(device_id=device_id):
-                # audio: 1‑D np.array @ MODEL_SAMPLE_RATE
-                # 1) high‑pass
-                audio_hp = sosfiltfilt(hp_sos, audio)
-                # 2) low‑pass
-                audio_f  = sosfiltfilt(lp_sos, audio_hp)
-
-                audio_queue.put(audio_f)
+                audio_queue.put(audio)
                 # Prints how long it took to get the classification                
                 print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
                 for label in labels:
