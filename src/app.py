316216d140a4c195aea074eb0a12eef4890aecbd
Amit "Sunny" Gupta <137424796+Amit-S-G@users.noreply.github.com>
2025-04-22 19:20:53 -0400

    Uncomment audio thread spawn

--- src/app.py
+++ src/app.py
@@ -253,7 +253,7 @@ if __name__ == '__main__':
 
         gevent.spawn(emit_data)
         gevent.spawn(emit_video_frames)
-        #gevent.spawn(audio_classification_loop)
+        gevent.spawn(audio_classification_loop)
         gevent.spawn(arduino_writer)
 
         socketio.run(app, host="0.0.0.0", port=8000, debug=False)
